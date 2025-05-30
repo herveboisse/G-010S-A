000be7e8c94decb7f1f61694eb70aa434dec866d
Hervé Boisse <admin@netgeek.ovh>
2025-05-30 11:50:50 +0200

    repack: add size check to avoid truncated image

--- repack.sh
+++ repack.sh
@@ -18,6 +18,15 @@ mksquashfs "$ROOTFS" patched.squashfs -noappend -comp xz -b 262144
 
 OUT="${IMAGE%.*}_patched.bin"
 cat "$DIR/uImage" patched.squashfs > "$OUT"
+sz="$(stat -c '%s' "${OUT}")"
+sz=$(( (6 << 20) - sz ))
+if [ ${sz} -lt 0 ]; then
+	echo "ERROR: image is too big of ${sz#-} bytes" >&2
+	rm "${OUT}"
+	exit 1
+else
+	echo "INFO: adding ${sz} bytes of padding" >&2
+fi
 dd if=/dev/null of="$OUT" bs=1 seek=6291456
 printf "\n%s created successfully\n" "$OUT"
 
